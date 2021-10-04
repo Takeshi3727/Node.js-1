50268a9059808af0c7a6ca7700a59761887058a0
Takeshi <takeshi.k3727@gmail.com>
2021-10-04 18:42:43 +0900

    array

--- play.js
+++ play.js
@@ -33,11 +33,20 @@ const person = {
   }
 }
 
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
+const toArray = (...args) => {
+  return args;
+}
+
+console.log(toArray(1, 2, 3, 4));
+
 const hobbies = ['Sports', 'Cooking'];
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
 // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
 // console.log(hobbies);
-hobbies.push('Programming');
-console.log(hobbies);
+const copiedArray = [...hobbies];
+console.log(copiedArray);
